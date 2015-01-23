127f22e95920c19f810498cd67a70bb21b70c768
chenxian0723 <178676873@qq.com>
2015-01-23 14:10:36 +0800

    Create Solution.cpp

---
+++ 016.3Sum-Closest/Solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int> &num, int target) {
+        sort(num.begin(),num.end());    //对数组排序
+        int n=num.size();
+        int dis=10000;    //设定一个最大距离
+        int res=0;
+        int j,k;    //可移动的首位下标
+        
+        for(int i=0;i<n-2;i++){
+            j=i+1;
+            k=n-1;
+            while(j<k){   //不断移动j和k
+                int temp=num[i]+num[j]+num[k]-target;
+                if(temp>0){
+                    k=k-1;
+                    if(temp<dis){
+                        dis=temp;
+                        res=temp+target;
+                    }
+                }
+                else{
+                    j=j+1;
+                    if(-temp<dis){
+                        dis=-temp;
+                        res=temp+target;
+                    }
+                }
+            }
+        }
+        
+        return res;
+    }
+};
