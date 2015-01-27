6d8e7927bf8e3226275f3222dc53c7539978bffa
chenxian0723 <178676873@qq.com>
2015-01-27 15:11:59 +0800

    Update Solution.cpp

--- 092.Reverse-Linked-List-II/Solution.cpp
+++ 092.Reverse-Linked-List-II/Solution.cpp
@@ -13,13 +13,13 @@ public:
         vhead->next=head;
         ListNode *pre=vhead;
         
-        for(int i=1;i<m;i++){   //寻找到开始反转的节点
+        for(int i=1;i<m;i++){       //寻找到开始反转的节点
             pre=head;
             head=head->next;
         }
         
         ListNode *temp=head;
-        for(int i=m;i<n;i++){     //将后面的点插入到前面，这部分感觉写得略繁琐，但一下子没想出更简洁的写法。。。
+        for(int i=m;i<n;i++){       //将后面的点插入到前面，这部分感觉写得略繁琐，但一下子没想出更简洁的写法。。。
             temp=pre->next;
             pre->next=head->next;
             head->next=head->next->next;
