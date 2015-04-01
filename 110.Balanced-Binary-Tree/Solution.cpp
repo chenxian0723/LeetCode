b2dc8e6d8863d07340a861e1cc5581f85071d800
chenxian0723 <178676873@qq.com>
2015-04-01 16:56:11 +0800

    Create Solution.cpp

---
+++ 110.Balanced-Binary-Tree/Solution.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode *root) {
+        if(root==NULL)
+            return true;
+        int depth=0;
+        return isBalancedTree(root,depth);
+    }
+    
+    bool isBalancedTree(TreeNode *root,int &depth){
+        if(root==NULL){
+            depth=0;    //注意要设置深度，空节点的深度为0
+            return true;
+        }
+        int left,right;
+        bool flag=isBalancedTree(root->left,left)&&isBalancedTree(root->right,right);
+        if((left-right>1)||(right-left>1))
+            return false;
+        depth=max(left,right)+1;    //设置深度
+        return flag;
+    }
+};
