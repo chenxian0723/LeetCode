e6866acd36b39854ee929ac1c85c02f941f2f281
chenxian0723 <178676873@qq.com>
2015-04-15 18:27:23 +0800

    Update Solution.cpp

--- 035.Search-Insert-Position/Solution.cpp
+++ 035.Search-Insert-Position/Solution.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int searchInsert(int A[], int n, int target) {
-        if(n==1)    //终止条件
-            if(target<=A[0])
-                return 0;
-            else
-                return 1;
+        if(target<=A[0])    //比最小的小或者比最大的大
+            return 0;
+        if(target>A[n-1])
+            return n;
         int mid=n/2;
         return searchInsert(A,mid,target)+searchInsert(A+mid,n-mid,target);
     }
