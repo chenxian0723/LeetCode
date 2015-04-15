320429d20f30484472f1331b87f91868151680ed
chenxian0723 <178676873@qq.com>
2015-04-15 16:56:42 +0800

    Update Solution.cpp

--- 007.Reverse-Integer/Solution.cpp
+++ 007.Reverse-Integer/Solution.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int reverse(int x) {
-        long long ans=0;    //为了方便在过程中记录真实数据
+        int result=0;
         int d;
-        
         while(x!=0){
             d=x%10;
             x=x/10;
-            ans=ans*10+d;
+            if((result>INT_MAX/10)||(result==INT_MAX/10)&&(d>7)         //越界的四种情况判断
+            ||(result<INT_MIN/10)||(result==INT_MIN/10)&&(d<-8))
+                return 0;
+            result=result*10+d;
         }
-        
-        if((ans>2147483647)||(ans<-2147483648))   //越界判断
-            ans=0;
-        return (int)ans;
+        return result;
     }
 };
