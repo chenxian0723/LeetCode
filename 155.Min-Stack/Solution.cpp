369d1e7e305825522f0afc12aa0d3d0f545faf42
chenxian0723 <178676873@qq.com>
2015-04-15 21:12:35 +0800

    Create Solution.cpp

---
+++ 155.Min-Stack/Solution.cpp
@@ -0,0 +1,26 @@
+class MinStack {
+public:
+    stack<int> s1;
+    stack<int> s2;
+
+    void push(int x) {
+        if(s1.empty())
+            s2.push(x);
+        else
+            s2.push(min(x,s2.top()));       //s2栈记录到当前位置时的最小值
+        s1.push(x);
+    }
+
+    void pop() {
+        s1.pop();
+        s2.pop();
+    }
+
+    int top() {
+        return s1.top();
+    }
+
+    int getMin() {
+        return min(s1.top(),s2.top());
+    }
+};
