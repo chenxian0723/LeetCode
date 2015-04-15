cdf81769bda4c406db9e62044a1b05a2dc7c951a
chenxian0723 <178676873@qq.com>
2015-04-15 15:37:12 +0800

    Update Solution.cpp

--- 001.Two-Sum/Solution.cpp
+++ 001.Two-Sum/Solution.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int> &numbers, int target) {
-        map<int,int> hm;
+        unordered_map<int,int> hm;
         int index1,index2;
         
         int num=numbers.size();
-        for(int i=0;i<num;i++){     //构建map
-            if(hm.find(target-numbers[i])!=hm.end()){     //这里如果有相同的值，只需记录一个，除非这两个数加起来刚好等于目标值
-                if(2*numbers[i]==target)
-                    return {hm[target-numbers[i]]+1,i+1};
+        for(int i=0;i<num;i++){
+            if(hm.find(target-numbers[i])!=hm.end()){       //找到了，输出
+                index1=hm[target-numbers[i]]+1;
+                index2=i+1;
+                if(index1<index2)
+                    return {index1,index2};
+                else
+                    return {index2,index1};
             }
-            else
+            else        //没找到，记录
                 hm[numbers[i]]=i;
         }
-        
-        for(int i=0;i<num;i++){     //遍历数组，寻找是否存在特定数
-            if(2*numbers[i]==target)    //为了防止在map中找到的数就是自身，因此直接跳过
-                continue;
-            if(hm.find(target-numbers[i])!=hm.end()){
-                index1=i+1;
-                index2=hm[target-numbers[i]]+1;
-                break;
-            }
-        }
-        
-        if(index1<index2)     //按大小顺序返回
-            return {index1,index2};
-        else
-            return {index2,index1};
     }
 };
